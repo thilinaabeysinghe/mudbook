b216fedbd832cbde7ab75157a4d60f6ffccb7641
thilina <thilinaabeysinghe@gmail.com>
2016-05-26 16:45:01 -0400

    Remove

--- mudbook.js
+++
@@ -1,9 +0,0 @@
-var page = new tabris.Page({
-  topLevel: true,
-  title: "MudBook"
-});
-new tabris.TextView({
-  layoutData: {centerX: 0, centerY: 0},
-  text: "Send Mud Photos to Facebook friends"
-}).appendTo(page);
-page.open();
